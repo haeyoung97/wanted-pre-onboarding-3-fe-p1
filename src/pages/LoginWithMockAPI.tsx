cd747e642c04ed55989a7c3eb5c6ba04eb6a5c9f
haeyoung97 <godud6379@naver.com>
2023-03-15 00:16:16 +0900

    feat: update mock api business logic

--- src/pages/LoginWithMockAPI.tsx
+++ src/pages/LoginWithMockAPI.tsx
@@ -1,56 +1,110 @@
-import React from 'react'
+import React, { useState } from "react";
 
-type LoginSuccessMessage = 'SUCCESS'
-type LoginFailMessage = 'FAIL'
+type LoginSuccessMessage = "SUCCESS";
+type LoginFailMessage = "FAIL";
 
 interface LoginResponse {
-  message: LoginSuccessMessage | LoginFailMessage
-  token: string
+  message: LoginSuccessMessage | LoginFailMessage;
+  token: string;
 }
 
-const login = async (username: string, password: string): Promise<LoginResponse | null> => {
-  // TODO: 올바른 username, password를 입력하면 {message: 'SUCCESS', token: (원하는 문자열)} 를 반환하세요.
-  return null
+interface UserInfo {
+  name: string;
 }
 
-const getUserInfo = async (): Promise<{ username: string } | null> => {
-  // TODO: login 함수에서 받은 token을 이용해 사용자 정보를 받아오세요.
-  return null
+interface User {
+  username: string;
+  password: string;
+  userInfo: UserInfo;
 }
+
+const users: User[] = [
+  {
+    username: "lee",
+    password: "test123",
+    userInfo: { name: "lee person" },
+  },
+  {
+    username: "park",
+    password: "test123",
+    userInfo: { name: "park person" },
+  },
+  {
+    username: "kim",
+    password: "test123",
+    userInfo: { name: "kim person" },
+  },
+];
+
+const _secretKey: string = "qwer1234!@#$";
+
+const login = async (
+  username: string,
+  password: string
+): Promise<LoginResponse | null> => {
+  const user: User | undefined = users.find((user: User) => {
+    return user.username === username && user.password === password;
+  });
+  return user
+    ? {
+        message: "SUCCESS",
+        token: JSON.stringify({ user: user.userInfo, secret: _secretKey }),
+      }
+    : null;
+};
+
+const getUserInfo = async (token: string): Promise<UserInfo | null> => {
+  const parsedToken = JSON.parse(token);
+  if (!parsedToken?.secret || parsedToken.secret !== _secretKey) return null;
+
+  const loggedUser: User | undefined = users.find((user: User) => {
+    return user.userInfo.name === parsedToken.user.name;
+  });
+
+  return loggedUser?.userInfo || null;
+};
 
 const LoginWithMockAPI = () => {
-  const loginSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+  const [userInfo, setUseInfo] = useState<UserInfo>({ name: "" });
+  const loginSubmitHandler = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
     event.preventDefault();
 
-    // TODO: form 에서 username과 password를 받아 login 함수를 호출하세요.
     const formData = new FormData(event.currentTarget);
-    for (let [key, value] of formData.entries()) console.log(key, value);
-  }
 
-  return (<div>
-    <h1>
-      Login with Mock API
-    </h1>
-    <form onSubmit={loginSubmitHandler}>
-      <label>
-        Username:
-        <input type="text" name="username"/>
-      </label>
-      <label>
-        Password:
-        <input type="password" name="password" />
-      </label>
-      <input type="submit" value="Submit" />
-      {/* TODO: 여기에 username과 password를 입력하는 input을 추가하세요. 제출을 위해 button도 추가하세요. */}
-    </form>
+    const loginRes = await login(
+      formData.get("username") as string,
+      formData.get("password") as string
+    );
+    if (!loginRes) return;
+
+    const userInfo = await getUserInfo(loginRes.token);
+    if (!userInfo) return;
+
+    setUseInfo(userInfo);
+  };
+
+  return (
     <div>
-      <h2>
-        User info
-      </h2>
-      {/* TODO: 유저 정보를 보여주도록 구현하세요. 필요에 따라 state나 다른 변수를 추가하세요. */}
-      {JSON.stringify({username: 'blueStragglr'})}
+      <h1>Login with Mock API</h1>
+      <form onSubmit={loginSubmitHandler}>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <div>
+        <h2>User info</h2>
+        {JSON.stringify(userInfo)}
+      </div>
     </div>
-  </div>)
-}
+  );
+};
 
-export default LoginWithMockAPI
+export default LoginWithMockAPI;
